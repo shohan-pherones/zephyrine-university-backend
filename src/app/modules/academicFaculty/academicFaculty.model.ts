a5b12889d7913b8ccf855f3cc55fcfdfb53cad2e
shohan-pherones <shohan.sub.56@gmail.com>
2024-05-28 21:17:11 +0600

    academic faculty interface, model and validations

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+import { TAcademicFaculty } from './academicFaculty.interface';
+
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: { ty: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const AcademicFaculty = model<TAcademicFaculty>(
+  'AcademicFaculty',
+  academicFacultySchema,
+);
+
+export default AcademicFaculty;
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod';
 
-const userValidationSchema = z.object({
-  password: z
-    .string({
-      invalid_type_error: 'Password must be string',
-    })
-    .max(20, 'Password must be at most 20 characters long')
-    .optional(),
+const createUserValidationSchema = z.object({
+  body: z.object({
+    password: z
+      .string({
+        invalid_type_error: 'Password must be string',
+      })
+      .max(20, 'Password must be at most 20 characters long')
+      .optional(),
+  }),
 });
 
-export default userValidationSchema;
+export const UserValidations = {
+  createUserValidationSchema,
+};
